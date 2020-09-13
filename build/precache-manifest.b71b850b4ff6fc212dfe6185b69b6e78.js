ad6cc1a755133a058ac13cf41233330e57f08b3c
Anish Prashun <1185211+Anishpras@users.noreply.github.com>
2020-09-13 05:34:40 +0530

    Build Complete

---
+++ build/precache-manifest.b71b850b4ff6fc212dfe6185b69b6e78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8a67c3a7229ecd5dfff9a5a171f716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b387e1723d5b2c310bc5",
+    "url": "/static/js/2.83032199.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.83032199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "182e7fee783d7c71710f",
+    "url": "/static/js/main.88546ec8.chunk.js"
+  },
+  {
+    "revision": "b4d12b70b7d90d24ec54",
+    "url": "/static/js/runtime-main.9496a8dc.js"
+  }
+]);
